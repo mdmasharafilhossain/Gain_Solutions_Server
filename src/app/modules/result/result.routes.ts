e145837bb14c9372b8a895acdba1f56ba942cbab
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-03-22 20:01:07 +0600

    Result Routes Added

---
+++ src/app/modules/result/result.routes.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import {create,getAll,update,remove,getByInstitute,getTopCourses,getTopStudents,performance,} from "./result.controller";
+import { validate } from "../../middlewares/validate";
+import {createResultSchema,updateResultSchema,} from "./result.schema";
+import { checkAuth } from "../../middlewares/checkAuth";
+
+const router = express.Router();
+router.post("/",checkAuth,validate(createResultSchema),create);
+router.get("/", checkAuth, getAll);
+router.patch("/:id",checkAuth,validate(updateResultSchema),update);
+router.delete("/:id", checkAuth, remove);
+router.get("/institute/:instituteId",checkAuth,getByInstitute);
+router.get("/top-courses",checkAuth,getTopCourses);
+router.get("/top-students",checkAuth,getTopStudents);
+router.get("/performance-test",checkAuth,performance);
+
+export default router;
