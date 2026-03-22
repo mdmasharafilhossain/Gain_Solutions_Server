2120168d11c0c1741a13de5883ba5c9c628879c9
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-03-22 12:30:49 +0600

    jwt added

---
+++ src/app/utils/jwt.ts
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken"
+
+export const generateToken = (payload: any) => {
+
+  return jwt.sign(payload, process.env.JWT_SECRET as string, {
+    
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  } as jwt.SignOptions);
+}
